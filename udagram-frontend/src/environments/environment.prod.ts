517295fe0d8ccf3339719d8f25f8c60ed41a25e2
Bao Huynh <scorers_punk0d@icloud.com>
2023-04-13 10:04:03 +0700

    Dev (#5)
    * update package-lock
    
    * Revert "update package-lock"
    
    This reverts commit 3e05ca673e886bac20fcb26c35771046953491a4.
    
    * Revert "update dockerfile"
    
    This reverts commit ff0c476fe3b26a5cb2c83ba9c1403b09c13b2194.
    
    * update package-lock
    
    * update deployment.yaml
    
    * update node 14
    
    * update package.json
    
    * update package.json

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad9201f00c57e4b09bca3d7127f9f76f-1348990730.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
